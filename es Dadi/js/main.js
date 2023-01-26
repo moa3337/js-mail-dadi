02579dcf0525e1c12f23f72bf96db474d0529b2f
moa3337 <moa-x@hotmail.com>
2023-01-26 15:16:13 +0100

    final commit gioco dadi

---
+++ es Dadi/js/main.js
@@ -0,0 +1,36 @@
+// PROGRAMMA PER SIMULARE IL GIOCO DEI DADI, GENERANDO
+// UN NUMERO RANDOMICO DA 1 A 6 PER L'UTENTE E PER IL
+// COMPUTER, IL NUMERO PIù ALTO VINCE!
+
+// funzione per il gioco dei dadi
+function diceGame() {
+
+    // genera un numero random tra 1 e 6 per il player
+    let player = Math.floor(Math.random() * 6) + 1;
+    console.log("Hai Fatto un " + player);
+    document.getElementById("player").innerHTML = "Hai Fatto un " + player;
+
+    // genera un numero random tra 1 e 6 per il computer
+    let computer = Math.floor(Math.random() * 6) + 1;
+    console.log("Il Computer ha fatto un " + computer);
+    document.getElementById("computer").innerHTML = "Il Computer ha fatto un " + computer;
+
+    // genera un vincitore
+    if (player > computer) {
+        console.log("Hai Vinto!");
+        document.getElementById("winner").innerHTML = "Hai Vinto!";
+    } else if (player < computer) {
+        console.log("Hai Perso!");
+        document.getElementById("winner").innerHTML = "Hai Perso!";
+    } else {
+        console.log("Pareggio!");
+        document.getElementById("winner").innerHTML = "Pareggio!";
+    }
+}
+
+// recupero del bottone
+let rollButton = document.getElementById("gioca");
+
+// funzione diceGame all'evento click
+rollButton.addEventListener("click", diceGame);
+
